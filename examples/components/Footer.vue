d759f7d909571d5388e2b34ad6b870a1a8dfb58d
wwhwwhwwh <819371938@qq.com>
2022-08-05 20:51:27 +0800

    doc version

--- examples/components/Footer.vue
+++ examples/components/Footer.vue
@@ -2,6 +2,8 @@
   <footer>
     <div class="container text-center">
       <p class="heart"></p>
+      <h4>{{ name }}：{{ version }} , {{ buildDataTime }}</h4>
+      <p class="heart"></p>
       <nav>
         <a class="nav-link" href="https://github.com/jcc/v-distpicker" target="_blank">GitHub</a>
         <a class="nav-link" href="https://pigjian.com" target="_blank">About</a>
@@ -10,3 +12,7 @@
     </div>
   </footer>
 </template>
+<script setup>
+import { version,name } from '../../package.json'
+const buildDataTime =new Date().toLocaleString()
+</script>
